b89fdf7a0be0b687164583e45375b4e9e7d1ca89
Rowan-Tech <matimurowan@gmail.com>
2026-04-21 03:46:46 +0200

    Initail upload

---
+++ public/assets/assets/js/pages-register-referral.js
@@ -0,0 +1,176 @@
+/**
+ *  Pages Authentication
+ */
+
+'use strict';
+const formAuthentication = document.querySelector('#formAuthentication');
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  (function () {
+    // Form validation for Add new record
+    if (formAuthentication) {
+      const fv = FormValidation.formValidation(formAuthentication, {
+        fields: {
+          reference_code: {
+            validators: {
+              notEmpty: {
+                message: 'Please enter your reference code'
+              },
+              stringLength: {
+                min: 8,
+                message: 'reference code must be more than 8 characters'
+              }
+            }
+          },
+          first_name: {
+            validators: {
+              notEmpty: {
+                message: 'Please enter First name'
+              },
+              stringLength: {
+                min: 2,
+                message: 'Last name must be more than 2 characters'
+              }
+            }
+          },
+          last_name: {
+            validators: {
+              notEmpty: {
+                message: 'Please enter last name'
+              },
+              stringLength: {
+                min: 2,
+                message: 'Last name must be more than 2 characters'
+              }
+            }
+          },
+          contact_number: {
+            validators: {
+              notEmpty: {
+                message: 'Please enter contact number'
+              },
+              stringLength: {
+                min: 8,
+                message: 'contact number  must be more than 6 characters'
+              }
+            }
+          },
+
+          email: {
+            validators: {
+              notEmpty: {
+                message: 'Please enter your email'
+              },
+              emailAddress: {
+                message: 'Please enter valid email address'
+              }
+            }
+          },
+
+          gender: {
+            validators: {
+              notEmpty: {
+                message: 'Select gender'
+              }
+            }
+          },
+          province: {
+            validators: {
+              notEmpty: {
+                message: 'Select province'
+              }
+            }
+          },
+
+          dob_day: {
+            validators: {
+              notEmpty: {
+                message: 'Select Day'
+              }
+            }
+          },
+          dob_month: {
+            validators: {
+              notEmpty: {
+                message: 'Select Month'
+              }
+            }
+          },
+          dob_year: {
+            validators: {
+              notEmpty: {
+                message: 'Select Year'
+              }
+            }
+          },
+
+          password: {
+            validators: {
+              notEmpty: {
+                message: 'Please enter your password'
+              },
+              stringLength: {
+                min: 6,
+                message: 'Password must be more than 6 characters'
+              }
+            }
+          },
+          password_confirmation: {
+            validators: {
+              notEmpty: {
+                message: 'Please confirm password'
+              },
+              identical: {
+                compare: function () {
+                  return formAuthentication.querySelector('[name="password"]').value;
+                },
+                message: 'The password and its confirm are not the same'
+              },
+              stringLength: {
+                min: 6,
+                message: 'Password must be more than 6 characters'
+              }
+            }
+          },
+          terms: {
+            validators: {
+              notEmpty: {
+                message: 'Please agree terms & conditions'
+              }
+            }
+          }
+        },
+        plugins: {
+          trigger: new FormValidation.plugins.Trigger(),
+          bootstrap5: new FormValidation.plugins.Bootstrap5({
+            eleValidClass: '',
+            rowSelector: '.mb-6'
+          }),
+          submitButton: new FormValidation.plugins.SubmitButton(),
+
+          defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+          autoFocus: new FormValidation.plugins.AutoFocus()
+        },
+        init: instance => {
+          instance.on('plugins.message.placed', function (e) {
+            if (e.element.parentElement.classList.contains('input-group')) {
+              e.element.parentElement.insertAdjacentElement('afterend', e.messageElement);
+            }
+          });
+        }
+      });
+    }
+
+    //  Two Steps Verification
+    const numeralMask = document.querySelectorAll('.numeral-mask');
+
+    // Verification masking
+    if (numeralMask.length) {
+      numeralMask.forEach(e => {
+        new Cleave(e, {
+          numeral: true
+        });
+      });
+    }
+  })();
+});

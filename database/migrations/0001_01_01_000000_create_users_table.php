d97aad5f0f96683b4e8a01689d07582b1634c712
Nare <mojapelonare@gmail.com>
2026-05-29 13:41:10 +0200

    modified:   database/migrations/0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     $table->string('province')->nullable();
     $table->string('reference_code')->nullable(); 
     $table->date('date_of_birth')->nullable();    
-
+    $table->enum('role', ['customer','student', 'admin'])->default('customer');
     $table->string('email')->unique();
     $table->timestamp('email_verified_at')->nullable();
     $table->string('password');
